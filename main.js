5810a7e3b01ffee4b26b94cfa11cc5c1547dd42a
jaiomkul <jaygupta2601@gmail.com>
2022-06-23 19:24:44 +0530

    add

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-import './style.css'
+// import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// document.querySelector('#app').innerHTML = `
+//   <h1>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
+
+import navbar from "./src/components/navBar.js";
+import logoImg from "./src/svg/svgImg.js";
+
+document.querySelector("#head-nav").innerHTML = navbar();
+
+document.querySelector(".logoImg").innerHTML = logoImg();
